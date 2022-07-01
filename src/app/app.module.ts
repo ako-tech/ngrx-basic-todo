453c6918fac9f748896582cc293e27ec12823ee5
akotech <akotech20@gmail.com>
2022-07-01 19:34:00 +0200

    added @ngrx/store-devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { TodosModule } from './todos/todos.module';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, TodosModule, StoreModule.forRoot({})],
+  imports: [BrowserModule, TodosModule, StoreModule.forRoot({}), StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })],
   providers: [],
   bootstrap: [AppComponent],
 })
