a55a721e844e067607a7fb5eebfe0a4081483973
akotech <akotech20@gmail.com>
2022-07-04 12:06:05 +0200

    change initialTodos to fixed values

--- src/app/todos/model/initial-todos.ts
+++ src/app/todos/model/initial-todos.ts
@@ -1,7 +1,19 @@
-import { createTodo } from './create-todo';
+import { Todo } from './todo.interface';
 
-export const initialTodos = [
-  'Escribir escaleta',
-  'Grabar Audio',
-  'Crear Clips',
-].map((description) => createTodo(description));
+export const initialTodos: Todo[] = [
+  {
+    id: 'c407874580eal',
+    description: 'Escribir escaleta',
+    completed: false,
+  },
+  {
+    id: 'e1495410c519a6',
+    description: 'Grabar Audio',
+    completed: false,
+  },
+  {
+    id: 'e2505508620180',
+    description: 'Crear Clips',
+    completed: false,
+  },
+];
